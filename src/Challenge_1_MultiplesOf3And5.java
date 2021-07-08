881c49db31acde069c86c742309430e7d500cedb
J0skin <jack15manning@gmail.com>
2021-07-08 14:43:41 +0100

    Completed Challenge 1

---
+++ src/Challenge_1_MultiplesOf3And5.java
@@ -0,0 +1,38 @@
+public class Challenge_1_MultiplesOf3And5
+{
+    /***
+     *      Challenge: 1
+     *      If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+     *      The sum of these multiples is 23.
+     *      Find the sum of all the multiples of 3 or 5 below 1000.
+     */
+
+    public static void main(String[] args)
+    {
+        int total = 0;
+        //For loop from 1 to 999
+        for (int i = 1; i < 1000; i++)
+        {
+            //Check if number is a multiple of 3
+            if (i % 3 == 0)
+            {
+                System.out.println(i + " is a multiple of 3");
+                total += i;
+            }
+            else
+            {
+                //Check if number is a multiple of 5
+                if (i % 5 == 0)
+                {
+                    System.out.println(i + " is a multiple of 5");
+                    total += i;
+                }
+                else
+                {
+                    System.out.println(i + " is not a multiple of 3 or 5");
+                }
+            }
+        }
+        System.out.println("Sum of multiples of 3 and 5 below 1000 is: " + total);
+    }
+}
--- src/Challenge_1_Multiples_of_3_and_5/Main.java
+++
@@ -1,4 +0,0 @@
-package Challenge_1_Multiples_of_3_and_5;
-
-public class Main {
-}
